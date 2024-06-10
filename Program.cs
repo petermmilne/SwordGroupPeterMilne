23a85bd2dd6c8e8fb7c31f3bd2cd2a391c7ae861
peter <peter@DESKTOP-PENE67C>
2024-06-10 13:57:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿if (args == null || args.Length == 0)
+{
+    Console.WriteLine("Please provide an input file");
+}
+else
+{
+    try
+    {
+        string caseSensitive = args[1];
+        StreamReader sr = new(args[0]);
+        Dictionary<string, int> map = [];
+        string input  = sr.ReadToEnd().Replace(" ", string.Empty).Replace("\t", string.Empty).ReplaceLineEndings("");
+        int totalCharsExSpecial = input.Length;
+
+        if (caseSensitive == null || caseSensitive == "0")
+        {
+            //  Group by character, order the result descending and case character and occurance to dictionary
+            map = input.GroupBy(x => x.ToString()).
+                     OrderByDescending(g => g.Count()).
+                        ToDictionary(g => g.Key, g => g.Count());
+        }
+        else if (caseSensitive == "1") // Ignore Case
+        {
+            //  Group by character, order the result descending and case character and occurance to dictionary
+            map = input.GroupBy(x => x.ToString(), StringComparer.InvariantCultureIgnoreCase).
+                    OrderByDescending(g => g.Count()).
+                        ToDictionary(g => g.Key, g => g.Count());
+        }
+
+        Console.WriteLine("Total Characters: " + totalCharsExSpecial);
+
+        //  Make sure we have 10 top results to show, if not then use the count
+        int topX = map.Count < 10 ? map.Count : 10;
+        var output = map.Take(topX).ToList();
+
+        if (topX == 0)
+        {
+            Console.WriteLine("No Results");
+            return;
+        }
+
+        for (int x=0; x < output.Count; x++)
+        {
+            //  If we are case sensistive we will use all lower case characters
+            var key = (caseSensitive != null && caseSensitive == "1") ? 
+                    output[x].Key.ToLowerInvariant() : output[x].Key.ToString();
+
+            Console.WriteLine(key + "(" + output[x].Value + ")");
+        }
+    }
+    catch (Exception e) { Console.WriteLine(e.ToString()); }
+}
---
+++ readme.txt
@@ -0,0 +1,13 @@
+﻿Simple application to count the number of characters in a text file
+
+Built using .NET 8
+
+Arguments:
+First : name of text file 
+Second (optional) : 0 or not provided = case sensitive, 1 = case insensitive
+
+Note:
+There is a pre-build event to copy input.txt to current configuration directory
+
+Please check out my website: http://www.petermilne.co.uk
+
